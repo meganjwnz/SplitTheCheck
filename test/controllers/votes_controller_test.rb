bcd1e2a35ead8c7302774c9b970771b20df0662f
mjones29@my.westga.edu <mjones29@my.westga.edu>
2019-04-14 00:45:54 -0400

    SPRINT 2 FINAL COMMIT. Added comments to tests

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -4,6 +4,7 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   fixtures :restaurants, :users
 
+  #creates an upvote and send back to restaurant page
   test "should get new upvote" do
      sign_in users(:example)
      get restaurants_url
@@ -11,6 +12,7 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
      assert_redirected_to "/restaurants"
   end
 
+  #creates a downvote and send back to restaurant page
   test "should get new downvote" do
      sign_in users(:example)
      get restaurants_url
@@ -18,12 +20,14 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
      assert_redirected_to "/restaurants"
   end
 
+  #does not allow unauthorized user to access upvotes
   test "should get not new upvote when not signed in" do
      get restaurants_url
      post votes_upvote_restaurant_path, params: { restaurant: restaurants(:one).id }
      assert_equal "/unauthenticated", path
   end
 
+  #does not allow unauthorized user to access downvotes
   test "should get not new downvote when not signed in" do
      get restaurants_url
      post votes_downvote_restaurant_path, params: { restaurant: restaurants(:one).id }
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -4,11 +4,13 @@ class VoteTest < ActiveSupport::TestCase
   include Devise::Test::IntegrationHelpers
   fixtures :restaurants, :users, :votes
 
+  #creates an upvote where a true = upvote
    test "should create upvote" do
      @upvote = Vote.upvote_restaurant(users(:example), restaurants(:one), true)
      assert_equal(true, @upvote.vote)
   end
 
+  #creates a downvote where a false = downvote
    test "should create downvote" do
      @downvote = Vote.upvote_restaurant(users(:example), restaurants(:one), false)
      assert_equal(false, @downvote.vote)
