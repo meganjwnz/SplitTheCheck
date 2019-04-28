d2ba0740ea48fffb89b44f16009954565e3e060a
mjones29@my.westga.edu <mjones29@my.westga.edu>
2019-04-28 13:34:40 -0400

    Comment controller authentication testing

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  fixtures :restaurants, :users, :favorites, :comments
+
   setup do
     @comment = comments(:one)
   end
+
+  #does not allow unauthorized user to access upvotes
+  test "should not allow comments when not signed in" do
+     get new_comment_url
+     assert_equal "/unauthenticated", path
+  end
+
 end
